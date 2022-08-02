1335366d75ba672426497134e4482b05196f8581
Minsoda <dg4res@gmail.com>
2022-08-02 17:22:43 +0900

    10815 숫자 카드 해결 완료

---
+++ 10815 숫자 카드/baekjoon_10815.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	vector<int> x(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> x[i];
+	}
+
+	int M;
+	cin >> M;
+	vector<int> y(M);
+
+	for (int i = 0; i < M; i++) {
+		cin >> y[i];
+	}
+
+	sort(x.begin(), x.end());
+
+	for (int i = 0; i < M; i++) {
+		if (binary_search(x.begin(), x.end(), y[i])) {
+			cout << "1 ";
+		}
+		else {
+			cout << "0 ";
+		}
+	}
+}
