606066a0a0fe96d3340919b48ca8ba579dea38c5
Minsoda <dg4res@gmail.com>
2023-03-19 17:56:55 +0900

    2532 먹이사슬 해결 완료

--- 17103 골드바흐 파티션/baekjoon_17103.cpp
+++ 17103 골드바흐 파티션/baekjoon_17103.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void plusI(string &i) {
+	int j = i.size() - 1;
+	while (j >= 0) {
+		if (i[j] == '9') {
+			i[j] = '0';
+		}
+		else {
+			i[j] = i[j] + 1;
+			break;
+		}
+		j--;
+	}
+	if (j == -1) {
+		i[0] = '1';
+		i += '0';
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+	string a, b;
+	cin >> a >> b;
 
+	int hap = 0;
+	bool isJJak = false;
+	int ans = 0;
+	while(true) {
+		hap = 0;
+		isJJak = false;
+		for (int i = 0, a_size = a.size(); i < a_size; i++) {
+			hap += a[i] - '0';
+			if (a[i] == '3' || a[i] == '6' || a[i] == '9') {
+				isJJak = true;
+				break;
+			}
+		}
+		if (hap % 3 == 0) isJJak = true;
+		if (isJJak) {
+			ans += 1;
+			ans %= 20150523;
+		}
+		if (a != b) {
+			plusI(a);
+		}
+		else {
+			break;
+		}
+	}
+	cout << ans;
 }
---
+++ 2532 먹이사슬/baekjoon_2532.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<pair<int, int>> x;
+vector<int> lis;
+
+void Print() {
+	for (int i = 0; i < lis.size(); i++) {
+		cout << lis[i] << ' ';
+	}
+	cout << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N;
+	x.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a >> x[i].first >> x[i].second;
+	}
+
+	sort(x.begin(), x.end(), [](pair<int,int> a, pair<int,int> b) {
+		if (a.first == b.first) return a.second > b.second;
+		else return a.first < b.first;
+		});
+
+	x.erase(unique(x.begin(), x.end()), x.end());
+
+	lis.push_back(-x[0].second);
+
+	for (int i = 1, x_size = x.size(); i < x_size; i++) {
+		if (-lis.back() >= x[i].second) {
+			lis.push_back(-x[i].second);
+		}
+		else {
+			int idx = upper_bound(lis.begin(), lis.end(), -x[i].second) - lis.begin();
+			lis[idx] = -x[i].second;
+		}
+	}
+
+	cout << lis.size();
+}
