7542462452dfd106b7ac3a36b17be021c00bab08
Minsoda <dg4res@gmail.com>
2023-04-23 17:55:12 +0900

    2481 해밍 경로

--- 2481 해밍 경로/baekjoon_2481.cpp
+++ 2481 해밍 경로/baekjoon_2481.cpp
@@ -3,11 +3,13 @@
 #include <string>
 #include <map>
 #include <cmath>
+#include <queue>
+#include <stack>
 using namespace std;
 
 int N, K, M;
-vector<string> x;
-map<int, bool> dic;
+vector<int> x;
+map<int, int> pos;
 
 int binaryTrans(string str) {
 	int ans = 0;
@@ -17,10 +19,6 @@ int binaryTrans(string str) {
 	return ans;
 }
 
-void BFS(int endPos) {
-
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
@@ -28,14 +26,51 @@ int main() {
 	x.resize(N);
 
 	for (int i = 0; i < N; i++) {
-		cin >> x[i];
-		dic.insert({ binaryTrans(x[i]),false });
+		string str;
+		cin >> str;
+		x[i] = binaryTrans(str);
+		pos[x[i]] = i;
 	}
 
 	cin >> M;
+	vector<vector<int>> graph(N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			int dir = x[i] ^ (1 << j);
+			if (pos.find(dir) != pos.end()) {
+				graph[i].push_back(pos[dir]);
+			}
+		}
+	}
+
+	queue<int> qu;
+	qu.push(0);
+	map<int, int> dic;
+	dic[0] = -1;
+	while (!qu.empty()) {
+		int dir = qu.front();
+		qu.pop();
+		for (int var : graph[dir]) {
+			if (dic.find(var) != dic.end()) continue;
+			qu.push(var);
+			dic[var] = dir;
+		}
+	}
 
 	for (int i = 0; i < M; i++) {
-		int endPos;
-		cin >> endPos;
+		int input;
+		cin >> input;
+		input--;
+		if (dic.find(input) == dic.end()) { cout << "-1\n"; continue; }
+
+		vector<int> ans;
+		while (input != -1) {
+			ans.push_back(input);
+			input = dic[input];
+		}
+
+		for (int j = ans.size() - 1; j >= 0; j--) cout << ans[j] + 1 << ' ';
+		cout << '\n';
 	}
 }
