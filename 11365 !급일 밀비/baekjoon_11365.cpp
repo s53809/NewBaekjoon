8ed35cb893c8cd8380f70ad52d0553519a5b1a15
Minsoda <dg4res@gmail.com>
2023-04-30 17:18:52 +0900

    Changes

---
+++ 11365 !급일 밀비/baekjoon_11365.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    while(true){
+        string str;
+        cin>>str;
+        if(str == "END") break;
+        for(int i = str.size() - 1;i >=0;i--){
+            cout<<str[i];
+        }
+        cout<<'\n';
+    }
+}
