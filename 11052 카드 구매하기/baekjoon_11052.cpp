cf4a749b67e5e3e1eaec0c6a40236aa2c9e183db
Minsoda <dg4res@gmail.com>
2023-05-07 14:56:13 +0900

    11052 카드 해결 완료

--- 11052 카드 구매하기/baekjoon_11052.cpp
+++ 11052 카드 구매하기/baekjoon_11052.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+int N;
+vector<int> x;
+vector<int> dp;
 
-	//!==
+int value(int num){
+    if(dp[num] != -1) return dp[num];
+    int maxNum = x[num];
+    for(int i = 1;i<=num / 2;i++){
+        maxNum = max(maxNum, value(num - i) + value(i));
+    }
+    dp[num] = maxNum;
+    return maxNum;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    cin>>N;
+    x.resize(N + 1);
+    dp.resize(N + 1);
+    for(int i = 1; i <= N; i++) {
+        cin>>x[i];
+        dp[i] = -1;
+    }
+    dp[N] = -1;
+
+    cout<<value(N);
 }
