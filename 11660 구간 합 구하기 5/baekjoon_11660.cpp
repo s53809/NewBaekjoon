32a844d15834b7fad106580a164bf888d6f547bb
Minsoda <dg4res@gmail.com>
2023-05-28 16:54:47 +0900

    11660 구간 합 구하기 5 해결 완료

--- 11660 구간 합 구하기 5/baekjoon_11660.cpp
+++ 11660 구간 합 구하기 5/baekjoon_11660.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	
+
 	int N, M;
 	cin >> N >> M;
 	vector<vector<int>> x(N + 1, vector<int>(N + 1, 0));
@@ -14,13 +14,6 @@ int main() {
 			cin >> x[i][j];
 			hap[i][j] = x[i][j] + (hap[i - 1][j] + hap[i][j - 1] - hap[i - 1][j - 1]);
 		}
-	}
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			cout << hap[i][j] << ' ';
-		}
-		cout << '\n';
 	}
 
 	for (int i = 0; i < M; i++) {
