7a1807263a0059bcec4b6faaa88fe51bf9e868c1
Minsoda <dg4res@gmail.com>
2023-08-01 14:36:43 +0900

    1799 비숍 해결 완료

--- testbox/소스.cpp
+++ testbox/소스.cpp
@@ -1,69 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <queue>
+#include <cstdio>
 using namespace std;
 
-int N, M, startNode;
-vector<vector<int>> x;
-vector<bool> visited;
-
-void DFS(int curNode) {
-	visited[curNode] = true;
-	cout << curNode << ' ';
-	for (int i = 0; i < x[curNode].size(); i++) {
-		if (!visited[x[curNode][i]]) DFS(x[curNode][i]);
-	}
-}
-
-void BFS(int startNode) {
-	queue<int> qu;
-	qu.push(startNode);
-	visited[startNode] = true;
-
-	while (!qu.empty()) {
-		int temp = qu.front();
-		qu.pop();
-		cout << temp << ' ';
-		for (int i = 0; i < x[temp].size(); i++) {
-			if (!visited[x[temp][i]]) { 
-				qu.push(x[temp][i]);
-				visited[x[temp][i]] = true;
-			}
-		}
-	}
-}
-
-void print() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j < x[i].size(); j++) {
-			cout << "x[" << i << "][" << j << "] = " << x[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+	vector<float> x(100);
 
-	cin >> N >> M >> startNode;
-	x.resize(N + 1);
-	visited.resize(N + 1, false);
-
-	for (int i = 0; i < M; i++) {
-		int strNode, endNode;
-		cin >> strNode >> endNode;
-		x[strNode].push_back(endNode);
-		x[endNode].push_back(strNode);
+	for (int i = 0; i < 100; i++) {
+		cin >> x[i];
 	}
 
-	for (int i = 1; i <= N; i++) {
-		sort(x[i].begin(), x[i].end());
+	cout << "[";
+	for (int i = 0; i < 100; i++) {
+		if (i == 99) printf("%.2f", x[i]);
+		else printf("%.2f, ", x[i]);
 	}
-
-	DFS(startNode);
-	cout << '\n';
-	fill(visited.begin(), visited.end(), false);
-	BFS(startNode);
+	cout << "]\n";
 }
