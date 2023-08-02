65e9b20cf3110bbc8c84347883e8efd8ade6d89e
Minsoda <dg4res@gmail.com>
2023-08-02 14:42:43 +0900

    12738 가장 긴 증가하는 부분 수열 3 해결 완료

---
+++ 12738 가장 긴 증가하는 부분 수열 3/baekjoon_12738.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	vector<int> x(N);
+	vector<int> leng;
+	for (int i = 0; i < N; i++) {
+		cin >> x[i];
+	}
+
+	leng.push_back(x[0]);
+	for (int i = 1; i < N; i++) {
+		if (*(leng.end() - 1) < x[i]) leng.push_back(x[i]);
+		else {
+			auto iter = lower_bound(leng.begin(), leng.end(), x[i]);
+			*iter = x[i];
+		}
+	}
+
+	cout << leng.size();
+}
