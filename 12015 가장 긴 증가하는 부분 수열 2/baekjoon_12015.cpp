ed351b97fcef76faabf563a9f3e636aacfb3bf42
Minsoda <dg4res@gmail.com>
2023-08-03 16:03:47 +0900

    12015 가장 긴 증가하는 부분 수열2 해결 완료

--- 12015 가장 긴 증가하는 부분 수열 2/baekjoon_12015.cpp
+++ 12015 가장 긴 증가하는 부분 수열 2/baekjoon_12015.cpp
@@ -9,20 +9,19 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> x(N);
-	vector<int> leng;
 	for (int i = 0; i < N; i++) {
 		cin >> x[i];
 	}
 
-	
+	vector<int> leng;
 	leng.push_back(x[0]);
+
 	for (int i = 1; i < N; i++) {
-		if (*(leng.end() - 1) < x[i]) leng.push_back(x[i]);
+		if (*(leng.end() - 1) < x[i])leng.push_back(x[i]);
 		else {
 			auto iter = lower_bound(leng.begin(), leng.end(), x[i]);
 			*iter = x[i];
 		}
 	}
-
 	cout << leng.size();
 }
