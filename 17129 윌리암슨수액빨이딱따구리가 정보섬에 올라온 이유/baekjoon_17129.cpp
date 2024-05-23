1997c1470c79323e44f9eb47fb33ea2ef0c1e891
DESKTOP-S84HTQO\Minseo Kim <dg4res@gmail.com>
2024-05-23 11:26:28 +0900

    17129 해결 완료

---
+++ 17129 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/baekjoon_17129.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int N, M;
+vector<vector<int>> x;
+vector<vector<int>> visited;
+int d_x[] = { 0,0,-1,1 };
+int d_y[] = { -1,1,0,0 };
+
+int BFS(int xPos, int yPos) {
+	queue<pair<int, int>> qu;
+	qu.push({ xPos, yPos });
+	visited[yPos][xPos] = 0;
+	while (!qu.empty()) {
+		pair<int, int> temp = qu.front();
+		qu.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int dir_x = temp.first + d_x[i];
+			int dir_y = temp.second + d_y[i];
+
+			if (0 <= dir_x && dir_x < M && 0 <= dir_y && dir_y < N) {
+				if (visited[dir_y][dir_x] != -1 || x[dir_y][dir_x] == 1) continue;
+				visited[dir_y][dir_x] = visited[temp.second][temp.first] + 1;
+				if (x[dir_y][dir_x] == 3) return visited[dir_y][dir_x];
+				qu.push({ dir_x, dir_y });
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+	cin >> N >> M;
+	x.resize(N, vector<int>(M));
+	visited.resize(N, vector<int>(M, -1));
+
+	int strXPos = 0, strYPos = 0;
+
+	for (int i = 0; i < N; i++) {
+		string str;
+		cin >> str;
+		for (int j = 0; j < M; j++) {
+			if (str[j] == '2') { strXPos = j; strYPos = i; }
+			else if (str[j] - '0' >= 3) str[j] = '3';
+			x[i][j] = str[j] - '0';
+		}
+	}
+
+	int ans = BFS(strXPos, strYPos);
+	if (ans == -1) cout << "NIE";
+	else {
+		cout << "TAK\n" << ans;
+	}
+	
+}
--- testbox/소스.cpp
+++ testbox/소스.cpp
@@ -1,27 +1,30 @@
 #include<iostream>
 using namespace std;
-typedef long long int lld;
+long long int N[101] = { 0,1,1,1,2,2 };
+long long int DP(long long int n) {
 
-int n;
-lld arr[101] = { 0,1,1,1,2,2 };
-lld fibo(long long a) {
-    if (arr[a] != -1) {
-        return arr[a];
+
+    if (N[n] != -1) {
+        return N[n];
     }
     else {
-        arr[a] = fibo(a - 1) + fibo(a - 5);
-        return  arr[a];
+
+        N[n] = DP(n - 2) + DP(n - 3);
+        return N[n];
     }
+
 }
-lld b;
 int main() {
-    for (int i = 6; i <= 100; i++) {
-        arr[i] = -1;
+    int T;
+    cin >> T;
+    for (int i = 6; i < 101; i++) {
+        N[i] = -1;
     }
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> b;
-        cout << fibo(b) << "\n";
+    DP(100);
+    for (int i = 0; i < T; i++) {
+        long long int n;
+        cin >> n;
+        cout << N[n] << "\n";
     }
 
 }
